e7fe64d7b1e573de9e2ad87a445e5d6bbe7d0b41
Ahmed Esmat <112887051+Zeus62@users.noreply.github.com>
2024-10-12 23:11:50 +0300

    Create Book.h

---
+++ Book.h
@@ -0,0 +1,21 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <string>
+using namespace std;
+
+class Book {
+public:
+    int id;
+    string title;
+    string author;
+    bool isAvailable;
+
+    Book(int id, string title, string author);
+
+    void display();
+
+    bool operator<(const Book& other) const;
+};
+
+#endif
